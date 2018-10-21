a6426f382d877baeb3b4762905152390d3c084a7
Ahmed Samara <asamara@ncsu.edu>
2018-10-21 13:23:57 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+cp bashrc ~/.bashrc
+cp vimrc ~/.vimrc
+cp gitconfig ~/.gitconfig
+
+
+# common packages I use.
+sudo apt install -y vim tmux python python-dev python-setuptools gcc libtinfo-dev zlib1g-dev
